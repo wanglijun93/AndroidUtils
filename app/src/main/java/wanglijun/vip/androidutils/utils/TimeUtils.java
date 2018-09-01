7e87246ac0d534c64c038e943e3b8455e3aa5d58
wanglijun93 <1402499306@qq.com>
2018-09-01 19:32:30 +0800

    添加时间工具类

---
+++ app/src/main/java/wanglijun/vip/androidutils/utils/TimeUtils.java
@@ -0,0 +1,579 @@
+/*
+ * Copyright 2016 北京博瑞彤芸文化传播股份有限公司
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wanglijun.vip.androidutils.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * @author wlj
+ * @date 2017/3/28
+ * @email wanglijundev@gmail.com
+ * @packagename wanglijun.vip.androidutils
+ * @description 时间工具类
+ */
+public class TimeUtils {
+
+    private TimeUtils() {
+        throw new UnsupportedOperationException("u can't fuck me...");
+    }
+
+    /**
+     * <p>在工具类中经常使用到工具类的格式化描述，这个主要是一个日期的操作类，所以日志格式主要使用 SimpleDateFormat的定义格式.</p>
+     * 格式的意义如下： 日期和时间模式 <br>
+     * <p>日期和时间格式由日期和时间模式字符串指定。在日期和时间模式字符串中，未加引号的字母 'A' 到 'Z' 和 'a' 到 'z'
+     * 被解释为模式字母，用来表示日期或时间字符串元素。文本可以使用单引号 (') 引起来，以免进行解释。"''"
+     * 表示单引号。所有其他字符均不解释；只是在格式化时将它们简单复制到输出字符串，或者在分析时与输入字符串进行匹配。
+     * </p>
+     * 定义了以下模式字母（所有其他字符 'A' 到 'Z' 和 'a' 到 'z' 都被保留）： <br>
+     * <table border="1" cellspacing="1" cellpadding="1" summary="Chart shows pattern letters, date/time component,
+     * presentation, and examples.">
+     * <tr>
+     * <th align="left">字母</th>
+     * <th align="left">日期或时间元素</th>
+     * <th align="left">表示</th>
+     * <th align="left">示例</th>
+     * </tr>
+     * <tr>
+     * <td><code>G</code></td>
+     * <td>Era 标志符</td>
+     * <td>Text</td>
+     * <td><code>AD</code></td>
+     * </tr>
+     * <tr>
+     * <td><code>y</code> </td>
+     * <td>年 </td>
+     * <td>Year </td>
+     * <td><code>1996</code>; <code>96</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>M</code> </td>
+     * <td>年中的月份 </td>
+     * <td>Month </td>
+     * <td><code>July</code>; <code>Jul</code>; <code>07</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>w</code> </td>
+     * <td>年中的周数 </td>
+     * <td>Number </td>
+     * <td><code>27</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>W</code> </td>
+     * <td>月份中的周数 </td>
+     * <td>Number </td>
+     * <td><code>2</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>D</code> </td>
+     * <td>年中的天数 </td>
+     * <td>Number </td>
+     * <td><code>189</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>d</code> </td>
+     * <td>月份中的天数 </td>
+     * <td>Number </td>
+     * <td><code>10</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>F</code> </td>
+     * <td>月份中的星期 </td>
+     * <td>Number </td>
+     * <td><code>2</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>E</code> </td>
+     * <td>星期中的天数 </td>
+     * <td>Text </td>
+     * <td><code>Tuesday</code>; <code>Tue</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>a</code> </td>
+     * <td>Am/pm 标记 </td>
+     * <td>Text </td>
+     * <td><code>PM</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>H</code> </td>
+     * <td>一天中的小时数（0-23） </td>
+     * <td>Number </td>
+     * <td><code>0</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>k</code> </td>
+     * <td>一天中的小时数（1-24） </td>
+     * <td>Number </td>
+     * <td><code>24</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>K</code> </td>
+     * <td>am/pm 中的小时数（0-11） </td>
+     * <td>Number </td>
+     * <td><code>0</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>h</code> </td>
+     * <td>am/pm 中的小时数（1-12） </td>
+     * <td>Number </td>
+     * <td><code>12</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>m</code> </td>
+     * <td>小时中的分钟数 </td>
+     * <td>Number </td>
+     * <td><code>30</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>s</code> </td>
+     * <td>分钟中的秒数 </td>
+     * <td>Number </td>
+     * <td><code>55</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>S</code> </td>
+     * <td>毫秒数 </td>
+     * <td>Number </td>
+     * <td><code>978</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>z</code> </td>
+     * <td>时区 </td>
+     * <td>General time zone </td>
+     * <td><code>Pacific Standard Time</code>; <code>PST</code>; <code>GMT-08:00</code> </td>
+     * </tr>
+     * <tr>
+     * <td><code>Z</code> </td>
+     * <td>时区 </td>
+     * <td>RFC 822 time zone </td>
+     * <td><code>-0800</code> </td>
+     * </tr>
+     * </table>
+     * <pre>
+     *                          HH:mm    15:44
+     *                         h:mm a    3:44 下午
+     *                        HH:mm z    15:44 CST
+     *                        HH:mm Z    15:44 +0800
+     *                     HH:mm zzzz    15:44 中国标准时间
+     *                       HH:mm:ss    15:44:40
+     *                     yyyy-MM-dd    2016-08-12
+     *               yyyy-MM-dd HH:mm    2016-08-12 15:44
+     *            yyyy-MM-dd HH:mm:ss    2016-08-12 15:44:40
+     *       yyyy-MM-dd HH:mm:ss zzzz    2016-08-12 15:44:40 中国标准时间
+     *  EEEE yyyy-MM-dd HH:mm:ss zzzz    星期五 2016-08-12 15:44:40 中国标准时间
+     *       yyyy-MM-dd HH:mm:ss.SSSZ    2016-08-12 15:44:40.461+0800
+     *     yyyy-MM-dd'T'HH:mm:ss.SSSZ    2016-08-12T15:44:40.461+0800
+     *   yyyy.MM.dd G 'at' HH:mm:ss z    2016.08.12 公元 at 15:44:40 CST
+     *                         K:mm a    3:44 下午
+     *               EEE, MMM d, ''yy    星期五, 八月 12, '16
+     *          hh 'o''clock' a, zzzz    03 o'clock 下午, 中国标准时间
+     *   yyyyy.MMMMM.dd GGG hh:mm aaa    02016.八月.12 公元 03:44 下午
+     *     EEE, d MMM yyyy HH:mm:ss Z    星期五, 12 八月 2016 15:44:40 +0800
+     *                  yyMMddHHmmssZ    160812154440+0800
+     *     yyyy-MM-dd'T'HH:mm:ss.SSSZ    2016-08-12T15:44:40.461+0800
+     * EEEE 'DATE('yyyy-MM-dd')' 'TIME('HH:mm:ss')' zzzz    星期五 DATE(2016-08-12) TIME(15:44:40) 中国标准时间
+     * </pre>
+     */
+    public static final SimpleDateFormat DEFAULT_SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+    public static final SimpleDateFormat CN_MM_DD_SDF = new SimpleDateFormat("MM月dd日", Locale.getDefault());
+    public static final SimpleDateFormat CN_M_D_SDF = new SimpleDateFormat("M月d日", Locale.getDefault());
+    public static final SimpleDateFormat EN_YYYY_MM_DD_SDF = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    public static final SimpleDateFormat CN_YYYY_MM_DD_SDF = new SimpleDateFormat("yyyy年MM月dd日", Locale.getDefault());
+    public static final SimpleDateFormat CN_YYYY_M_D_SDF = new SimpleDateFormat("yyyy年M月d日", Locale.getDefault());
+    public static final SimpleDateFormat EN_M_D_SDF = new SimpleDateFormat("M/d", Locale.getDefault());
+    public static final SimpleDateFormat EN_MM_DD_SDF = new SimpleDateFormat("MM-dd", Locale.getDefault());
+    public static final SimpleDateFormat EN_HH_mm = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+
+    /**
+     * 将时间戳转为时间字符串
+     * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+     *
+     * @param milliseconds 毫秒时间戳
+     * @return 时间字符串
+     */
+    public static String milliseconds2String(long milliseconds) {
+        return milliseconds2String(milliseconds, DEFAULT_SDF);
+    }
+
+    /**
+     * 将时间戳转为时间字符串
+     * <p>格式为用户自定义</p>
+     *
+     * @param milliseconds 毫秒时间戳
+     * @param format       时间格式
+     * @return 时间字符串
+     */
+    public static String milliseconds2String(long milliseconds, SimpleDateFormat format) {
+        return format.format(new Date(milliseconds));
+    }
+
+    /**
+     * 将时间字符串转为时间戳
+     * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+     *
+     * @param time 时间字符串
+     * @return 毫秒时间戳
+     */
+    public static long string2Milliseconds(String time) {
+        return string2Milliseconds(time, DEFAULT_SDF);
+    }
+
+    /**
+     * 将时间字符串转为时间戳
+     * <p>格式为用户自定义</p>
+     *
+     * @param time   时间字符串
+     * @param format 时间格式
+     * @return 毫秒时间戳
+     */
+    public static long string2Milliseconds(String time, SimpleDateFormat format) {
+        try {
+            return format.parse(time).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * 将时间字符串转为Date类型
+     * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+     *
+     * @param time 时间字符串
+     * @return Date类型
+     */
+    public static Date string2Date(String time) {
+        return string2Date(time, DEFAULT_SDF);
+    }
+
+
+    /**
+     * 将时间字符串转为Date类型
+     * <p>格式为用户自定义</p>
+     *
+     * @param time   时间字符串
+     * @param format 时间格式
+     * @return Date类型
+     */
+    public static Date string2Date(String time, SimpleDateFormat format) {
+        return new Date(string2Milliseconds(time, format));
+    }
+
+    /**
+     * 将Date类型转为时间字符串
+     * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+     *
+     * @param time Date类型时间
+     * @return 时间字符串
+     */
+    public static String date2String(Date time) {
+        return date2String(time, DEFAULT_SDF);
+    }
+
+    /**
+     * 将Date类型转为时间字符串
+     * <p>格式为用户自定义</p>
+     *
+     * @param time   Date类型时间
+     * @param format 时间格式
+     * @return 时间字符串
+     */
+    public static String date2String(Date time, SimpleDateFormat format) {
+        return format.format(time);
+    }
+
+    /**
+     * 将Date类型转为时间戳
+     *
+     * @param time Date类型时间
+     * @return 毫秒时间戳
+     */
+    public static long date2Milliseconds(Date time) {
+        return time.getTime();
+    }
+
+    /**
+     * 将时间戳转为Date类型
+     *
+     * @param milliseconds 毫秒时间戳
+     * @return Date类型时间
+     */
+    public static Date milliseconds2Date(long milliseconds) {
+        return new Date(milliseconds);
+    }
+
+
+
+
+
+
+
+
+    /**
+     * 获取两个毫秒值之间的时间差(天数)
+     *
+     * @param time1
+     * @param time2     一般time2写较大的值
+     * @return
+     */
+    public static int getIntervalTime(long time1, long time2){
+        return (int) ((time2 - time1) / (1000 * 3600 * 24));
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @return 毫秒时间戳
+     */
+    public static long getCurTimeMills() {
+        return System.currentTimeMillis();
+    }
+
+    /**
+     * 获取当前时间
+     * <p>格式为yyyy-MM-dd HH:mm:ss</p>
+     *
+     * @return 时间字符串
+     */
+    public static String getCurTimeString() {
+        return date2String(new Date());
+    }
+
+    /**
+     * 获取当前时间
+     * <p>格式为用户自定义</p>
+     *
+     * @param format 时间格式
+     * @return 时间字符串
+     */
+    public static String getCurTimeString(SimpleDateFormat format) {
+        return date2String(new Date(), format);
+    }
+
+    /**
+     * 获取当前时间
+     * <p>Date类型</p>
+     *
+     * @return Date类型时间
+     */
+    public static Date getCurTimeDate() {
+        return new Date();
+    }
+
+
+
+    /**
+     * 判断闰年
+     *
+     * @param year 年份
+     * @return {@code true}: 闰年<br>{@code false}: 平年
+     */
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    /**
+     * 获取当前日期是星期几<br>
+     *
+     * @return 当前日期是星期几
+     */
+    public static String getWeekOfDate() {
+        String[] weekDays = {"周日", "周一", "周二", "周三", "周四", "周五", "周六"};
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        if (w < 0)
+            w = 0;
+
+        return weekDays[w];
+    }
+
+    /**
+     * 获取当前日期是星期几<br>
+     *
+     * @return 当前日期是星期几
+     */
+    public static String getWeek() {
+        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        if (w < 0)
+            w = 0;
+
+        return weekDays[w];
+    }
+
+    /**
+     * 获取当前日期的下一天是星期几<br>
+     *
+     * @return 当前日期是星期几
+     */
+    public static String getWeekOfNextDate() {
+        String[] weekDays = {"周日", "周一", "周二", "周三", "周四", "周五", "周六"};
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+
+        int w = cal.get(Calendar.DAY_OF_WEEK);
+        if (w == 0)
+            w = 1;
+        if (w > 6)
+            w = 0;
+        return weekDays[w];
+    }
+
+
+    /**
+     * 获取桌面时钟的时间
+     */
+    public static String getDeskClockTime() {
+        final Calendar c = Calendar.getInstance();
+        c.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        String mHuor = String.valueOf(c.get(Calendar.HOUR_OF_DAY));
+        String imnute = String.valueOf(c.get(Calendar.MINUTE));
+        if (imnute.length() < 2) {
+            imnute = "0" + imnute;
+        }
+        if (mHuor.length() < 2) {
+            mHuor = "0" + mHuor;
+        }
+        return mHuor + ":" + imnute;
+    }
+
+    /**
+     * 获取当前月份是几月
+     */
+    public static String getCurMonth() {
+
+        Calendar c = Calendar.getInstance();
+
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        String monthString = null;
+
+        if (month < 10) {
+
+            monthString = 0 + "" + month;
+        } else {
+
+            monthString = "" + month;
+        }
+
+        /**
+         * 月份从0开始计算
+         */
+        return year + "-" + monthString;
+    }
+
+    /**
+     * 获取两个月份差值
+     *
+     * @param month1
+     * @param month2
+     * @return
+     */
+    public static int getDistanceOfTwoMonth(String month1, String month2) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+        Calendar bef = Calendar.getInstance();
+        Calendar aft = Calendar.getInstance();
+        bef.setTime(sdf.parse(month1));
+        aft.setTime(sdf.parse(month2));
+        int result = aft.get(Calendar.MONTH) - bef.get(Calendar.MONTH);
+        int month = (aft.get(Calendar.YEAR) - bef.get(Calendar.YEAR)) * 12;
+
+        return Math.abs(month + result);
+    }
+
+    /**
+     * 判断旧日期和新日期【新旧根据获取先后而定】的先后关系，如果新日期大于旧日期则返回true
+     */
+    public static boolean compareDates(int oldYear, int oldMonth, int oldDay,
+                                       int newYear, int newMonth, int newDay) {
+        if (newYear != oldYear)
+            return !(newYear < oldYear);
+
+        if (newMonth != oldMonth)
+            return !(newMonth < oldMonth);
+
+        if (newDay != oldDay)
+            return !(newDay < oldDay);
+
+        return false;
+    }
+
+    /**
+     * 获取当前时
+     */
+    public static int getHour() {
+        final Calendar c = Calendar.getInstance();
+        c.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        return c.get(Calendar.HOUR_OF_DAY);
+    }
+
+    /**
+     * 获取指定时间的小时
+     */
+    public static int getHour(long timeMills) {
+        final Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(timeMills);
+        c.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        return c.get(Calendar.HOUR_OF_DAY);
+    }
+
+
+
+    /**
+     * 获取当前月的天数
+     *
+     * @return
+     */
+    public static int getCurMonthLength() {
+        Calendar a = Calendar.getInstance();
+        a.set(Calendar.DATE, 1);
+        a.roll(Calendar.DATE, -1);
+        int maxDate = a.get(Calendar.DATE);
+        return maxDate;
+    }
+
+    /**
+     * 获取当前日期
+     *
+     * @return
+     */
+    public static String getCurDayOFMonth() {
+        Calendar c = Calendar.getInstance();
+        return String.valueOf(c.get(Calendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * 获取当前年份
+     * @return
+     */
+    public static String getCurYear() {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        return year+"";
+    }
+}
